8768ca97ae1f60bbb38253b19c8b6a751220ceb3
jsdario <jesus@netbeast.co>
2018-04-27 11:52:10 +0200

    Improved script

--- build-openwrt-target.sh
+++ build-openwrt-target.sh
@@ -1,7 +1,7 @@
-OPEN_WRT_PATH="/Volumes/OpenWrt/openwrt"
+export OPEN_WRT_PATH="/Volumes/OpenWrt/openwrt"
 #These depend on your arch. Look it up on your build folder.
-ARCH="mipsel-openwrt-linux-musl"
-TOOLCHAIN="toolchain-mipsel_24kc_gcc-7.3.0_musl"
+export ARCH="mipsel-openwrt-linux"
+export TOOLCHAIN="toolchain-mipsel_24kc_gcc-7.3.0_musl"
 
 export AR=${OPEN_WRT_PATH}/staging_dir/${TOOLCHAIN}/bin/${ARCH}-ar
 export CC=${OPEN_WRT_PATH}/staging_dir/${TOOLCHAIN}/bin/${ARCH}-gcc
@@ -11,3 +11,11 @@ export RANLIB=${OPEN_WRT_PATH}/staging_dir/${TOOLCHAIN}/bin/${ARCH}-ranlib
 export STAGING_DIR=${OPEN_WRT_PATH}/staging_dir
 export LIBPATH=${OPEN_WRT_PATH}/staging_dir/toolchain-${TOOLCHAIN}/lib
 export LDFLAGS='-Wl,-rpath-link '${LIBPATH}
+
+
+./configure --without-snapshot --without-npm --without-intl --fully-static
+make
+
+
+# Use this to connect to the qemu vm
+# scp -r jsdario@192.168.0.105:/Volumes/OpenWrt/node .
